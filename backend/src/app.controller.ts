405eeb47ec47796d5e4bc15c1a6d406dea6ae1e6
Solomon Razaq <souls1@live.co.za>
2024-02-13 17:19:17 -0700

    chore(backend): fix linting issues

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Get, Controller } from "@nestjs/common";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  root(): string {
+    return "Welcome to my super secret API. I hope you find what you're looking for :)";
   }
 }
