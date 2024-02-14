6defdbcbd7f3cd0d13ffe2420a4543232d19457a
Solomon Razaq <souls1@live.co.za>
2024-02-14 00:30:11 -0700

    chore(frontend): integrate material ui 5.15

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "../theme";
 
 export default function RootLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-      <html lang="en">
-        <body>{children}</body>
-      </html>
-    )
-  }
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
+    </html>
+  );
+}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@mui/material";
+
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>
-  }
+  return (
+    <>
+      <h1>Hello, Next.js!</h1>
+      <Button variant="contained">MY BUTTON</Button>
+    </>
+  );
+}
